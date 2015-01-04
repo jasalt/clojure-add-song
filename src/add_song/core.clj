4849a42865f66339431f19c8ce82c277249d7dc1
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-04 19:38:17 +0200

    Read yaml file. Add yaml file for testing.

--- src/add_song/core.clj
+++ src/add_song/core.clj
@@ -1,21 +1,29 @@
 (ns add-song.core
   "Raw clojure code."
   (:require [net.cgrand.enlive-html :as enlive]
-            [clojure.java.io        :as java]
+            [clojure.java.io        :as io]
+            [clojure.edn            :as edn]
             [clojure.string         :as string]
             [clj-yaml.core          :as yaml]
             :verbose))
 
+
+(defn read-stations-file
+  "Read stations from yaml-file"
+  [filename]
+  (-> filename io/resource slurp yaml/parse-string)
+  )
+
 (defn scrape-somafm
-  "Get currently playing song from groove salad"
+  "Get currently playing song from Groove Salad"
   []
-  (let [html (enlive/html-resource (java/as-url
+  (let [html (enlive/html-resource (io/as-url
                                     "http://somafm.com/groovesalad/songhistory.html"))
         song-elem (nth (enlive/select html [:tr]) 2)
         info-texts (map enlive/text
                         (enlive/select song-elem [:td]))]
 
-    {:time (first (string/split (nth info-texts 0) #" "))
+    {:station-time (first (string/split (nth info-texts 0) #" "))
      :artist (nth info-texts 1)
      :title (nth info-texts 2)
      :album (nth info-texts 3)
@@ -23,8 +31,8 @@
     )
   )
 
-
 (defn -main
   [& args]
   (println (scrape-somafm))
+  (println (read-stations-file "./radio-stations.yaml"))
   )
