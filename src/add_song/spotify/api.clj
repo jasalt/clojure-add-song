8e81f151a5cd1fadea27c5bdfd80346791d02526
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-07 22:39:03 +0200

    Save tokens to file. Cleanup.

--- src/add_song/spotify/api.clj
+++ src/add_song/spotify/api.clj
@@ -1,5 +1,7 @@
 (ns add-song.spotify.api
-  "Spotify API authentication etc."
+  "Spotify API authentication and operations.
+  Spotify authorization code login as in
+  https://developer.spotify.com/web-api/authorization-guide/"
   (:require
    [clj-http.client        :as client]
    [clojure.data.json      :as json]
@@ -17,7 +19,12 @@
 ;; leiningen plugin
 (def spotify-client-id (env :spotify-client-id))
 (def spotify-client-secret (env :spotify-client-secret))
+
 (def callback-uri "http://localhost:8080/callback")
+
+(def token-cache-dir (str (env :home) "/.add-song/"))
+(def token-cache-file (str (env :home) "/.add-song/tokens"))
+
 
 (import [java.net URLEncoder])
 (defn encode-params [request-params]
@@ -25,14 +32,6 @@
   (let [encode #(URLEncoder/encode (str %) "UTF-8")
         coded (for [[n v] request-params] (str (encode n) "=" (encodev)))]
     (apply str (interpose "&" coded))))
-
-(defn search-tracks
-  "Search track from public Spotify web api"
-  [artist title]
-  (-> (client/get
-       (str "http://ws.spotify.com/search/1/track.json?q="
-            (str artist " " title)))
-      (:body) (json/read-str :key-fn keyword)) )
 
 (defn serve-callback-page
   "Serve callback page to which Spotify login redirects.
@@ -72,10 +71,11 @@
   )
 
 (defn auth-request
-  "First authorization step described in
-  https://developer.spotify.com/web-api/authorization-guide/"
+  "Request authorization code
+  (1) Send user to authorization page (2) user logs in and gives access
+  (3) user is redirected to callback and authorization-code is received"
   []
-  ;; Send user to do authentication in web browser
+  (println "Please log in to Spotify and give permission.")
   (browser/browse-url (str "https://accounts.spotify.com/authorize?"
                            (encode-params
                             {"client_id" spotify-client-id
@@ -85,20 +85,20 @@
                                           "playlist-modify-private "
                                           "playlist-modify-public")})))
 
-  (println "Running callback-server on localhost.")
-  (println "Please log in and agree to permissions.")
-
+  (println (str "Starting callback-server on " callback-uri))
+  ;; Wait for user to be redirected to callback
   ;; Extract response code from query string
   (get ((ring-params/assoc-query-params (serve-callback-page) "UTF-8")
         :query-params) "code"))
 
 (defn token-request
+  "(4) Swap authorization-code for access and refresh tokens"
   [authorization-code]
-  ;;spotify-client-id
   (let [auth-response (client/post "https://accounts.spotify.com/api/token"
-                                   {:form-params {:grant_type "authorization_code"
-                                                  :code authorization-code
-                                                  :redirect_uri callback-uri}
+                                   {:form-params
+                                    {:grant_type "authorization_code"
+                                     :code authorization-code
+                                     :redirect_uri callback-uri}
                                     :headers
                                     {"Authorization"
                                      (str "Basic "
@@ -108,17 +108,62 @@
     (println "Received auth token")
     (-> auth-response (:body) (json/read-str :key-fn keyword))))
 
-;;(def auth-tokens (token-request (auth-request)))
-;;auth-tokens
-
-(defn oauth-login
+(defn fetch-new-tokens
+  "Fetch new tokens."
   []
   (let [auth-code (auth-request)
-        auth-tokens (token-request auth-code)])
+        auth-tokens (token-request auth-code)]
+
+    (println "Saving received token")
+    (pprint auth-tokens)
+
+    (if (false? (.isDirectory (io/file token-cache-dir)))
+      (.mkdir (io/file token-cache-dir)))
+
+    (spit token-cache-file auth-tokens)
+    ;; TODO save date
+    )
   )
+
+(defn refresh-tokens
+  "(6) Use refresh token to get new access token"
+  []
+  nil ;;TODO
+  )
+
+(defn read-saved-tokens
+  "Read saved access tokens from file"
+  []
+  (try
+    (read-string (slurp token-cache-file))
+    (catch Exception e (println "No saved tokens found") nil))
+  )
+
+(defn give-tokens
+  "Hands a working Spotify access token, fetches new one if missing."
+  []
+  (let [old-tokens (read-saved-token)]
+    (if old-tokens
+      old-tokens ;; TODO Check if old-tokens have dried out
+      (fetch-new-tokens)
+      )
+    )
+  )
+
+
+;; API operations
+;; TODO separate namespaces
 
 (defn add-to-inbox
   "Add song to Inbox-playlist, create it if not existing"
   [spotify-uri]
   nil ;; TODO
   )
+
+(defn search-tracks
+  "Search track from public Spotify web api"
+  [artist title]
+  (-> (client/get
+       (str "http://ws.spotify.com/search/1/track.json?q="
+            (str artist " " title)))
+      (:body) (json/read-str :key-fn keyword)))
