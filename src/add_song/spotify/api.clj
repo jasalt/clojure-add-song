908062e21bfd6125a4839645833a38694f27a11e
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-08 16:13:03 +0200

    GET wrapper arity. Allow passing optional params.

--- src/add_song/spotify/api.clj
+++ src/add_song/spotify/api.clj
@@ -18,10 +18,14 @@
 
 (defn get-private
   "GET for private data"
-  [api-endpoint]
-  (client/get (str spotify-api-url api-endpoint)
-              {:oauth-token (auth/get-access-token)
-               :throw-entire-message? true}))
+  ([api-endpoint opt-params]
+   (client/get (str spotify-api-url api-endpoint)
+               (merge {:oauth-token (auth/get-access-token)
+                       :throw-entire-message? true} opt-params)))
+  ([api-endpoint]
+   (get-private api-endpoint nil)
+   )
+  )
 
 (defn get-user-id
   "Give current users Spotify user_id"
@@ -30,7 +34,8 @@
 (defn list-user-playlists
   [user-id]
   ;;TODO paginate through all
-  (-> (get-private (str "/users/" user-id "/playlists"))
+  (-> (get-private (str "/users/" user-id "/playlists")
+                   {:query-params {"limit" "50"}})
       parse-body-json))
 
 (defn playlist-exists
