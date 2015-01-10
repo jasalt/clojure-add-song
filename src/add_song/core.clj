863aa9227316325034acc469b90a59ec5b26fe93
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2015-01-10 22:56:29 +0200

    Fix crash when scrape-station returns nil.

--- src/add_song/core.clj
+++ src/add_song/core.clj
@@ -7,11 +7,10 @@
 
    ;; Own modules/namespaces
    [add-song.spotify.api   :as spotify]
-   
+
    add-song.scrapers.somafm
    add-song.scrapers.dnbradio
    )(:gen-class))
-
 
 (defn read-parse-yaml
   "Read and parse yaml-file from resources folder"
@@ -28,9 +27,9 @@
          :dnbradio.com #(add-song.scrapers.dnbradio/now-playing %)}
 
         scraper (get station-network-scrapers
-                     (keyword (station :network)))]
-
-    (merge (scraper station) station)
+                     (keyword (station :network)))
+        scrape-result (scraper station)]
+    (if scrape-result (merge station scrape-result))
     )
   )
 
@@ -42,7 +41,7 @@
                      (filter identity) first)]
     (if station
       (scrape-station station)
-      station
+      nil
       )
     )
   )
@@ -71,22 +70,25 @@
                 " @ " (result :network) " " (result :station)))
   )
 
+(defn add-station-now-playing
+  [station-acronym]
+  (let [result
+        (do (process-input station-acronym
+                           (read-parse-yaml "./radio-stations.yaml")))]
+    (if result
+      (do
+        (post-song (result :artist) (result :title))
+        (print-success result)
+        )
+      (println "Didn't get anything to add.."))))
+
 (defn -main
   "Does simple validation for command argument and does stuff with it.
   TODO refactor."
   [& args]
 
   (if (= (count args) 1)
-    (let [result
-          (do (process-input (first args)
-                             (read-parse-yaml "./radio-stations.yaml")))]
-      (if result
-        (do (post-song (result :artist) (result :title))
-            (print-success result)
-            )
-
-        (print-help))
-      )
+    (add-station-now-playing (first args))
     (print-help)
     )
   )
